0049a66f3e062efd4dc71c774acee6b27325c52c
LeandroFrancaUS <101406453+LeandroFrancaUS@users.noreply.github.com>
2025-07-13 18:50:00 -0300

    deployment

--- src/app/sitemap/route.ts
+++ src/app/sitemap/route.ts
@@ -4,10 +4,12 @@ export async function GET() {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-      <loc>https://solarinvest.info/</loc>
+      <loc>https://solarinves.info/</loc>
       <lastmod>${new Date().toISOString()}</lastmod>
     </url>
-    <!-- Adicione outras URLs aqui -->
+    <loc>https://solarinvest.com/</loc>
+    <loc>https://www.instagram.com/solarinvest.br/</loc>
+    
   </urlset>`;
 
   return new Response(sitemap, {
