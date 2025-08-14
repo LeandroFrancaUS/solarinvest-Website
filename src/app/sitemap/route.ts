541382152d6a5952e19dd6a3de71fdcdb9cf2e3e
Leandro Franca <101406453+LeandroFrancaUS@users.noreply.github.com>
2025-08-14 00:21:24 -0300

    fix sitemap domain

--- src/app/sitemap/route.ts
+++ src/app/sitemap/route.ts
@@ -4,7 +4,7 @@ export async function GET() {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-      <loc>https://solarinves.info/</loc>
+      <loc>https://solarinvest.info/</loc>
       <lastmod>${new Date().toISOString()}</lastmod>
     </url>
     <loc>https://solarinvest.com/</loc>
