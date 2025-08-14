1e78d81fde8ee282e1f4f88fbde01fa491311459
Leandro Franca <101406453+LeandroFrancaUS@users.noreply.github.com>
2025-08-14 00:55:59 -0300

    fix: update sitemap urls

--- src/app/sitemap/route.ts
+++ src/app/sitemap/route.ts
@@ -1,16 +1,15 @@
-// src/app/sitemap.xml/route.ts
-
 export async function GET() {
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>https://solarinves.info/</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-    </url>
-    <loc>https://solarinvest.com/</loc>
-    <loc>https://www.instagram.com/solarinvest.br/</loc>
-    
-  </urlset>`;
+  const baseUrl = 'https://solarinvest.info';
+  const lastMod = new Date().toISOString();
+  const pages = ['/', '/comofunciona', '/sobre', '/solucoes', '/contato'];
+
+  const urls = pages
+    .map(
+      (page) => `    <url>\n      <loc>${baseUrl}${page}</loc>\n      <lastmod>${lastMod}</lastmod>\n    </url>`,
+    )
+    .join('\n');
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls}\n  </urlset>`;
 
   return new Response(sitemap, {
     headers: {
@@ -18,3 +17,4 @@ export async function GET() {
     },
   });
 }
+
