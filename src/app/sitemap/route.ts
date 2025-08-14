7af196f711fa6fddfec6ba78c351dbfb0b3db771
Leandro Franca <101406453+LeandroFrancaUS@users.noreply.github.com>
2025-08-14 00:45:15 -0300

    Fix sitemap domain and structure

--- src/app/sitemap/route.ts
+++ src/app/sitemap/route.ts
@@ -1,20 +1,28 @@
-// src/app/sitemap.xml/route.ts
+// src/app/sitemap/route.ts
 
 export async function GET() {
+  const baseUrl = "https://solarinvest.info";
+
+  const routes = ["/", "/sobre", "/solucoes", "/contato", "/comofunciona"];
+
+  const urls = routes
+    .map(
+      (route) => `
+    <url>
+      <loc>${baseUrl}${route}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+    </url>`
+    )
+    .join("");
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>https://solarinves.info/</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-    </url>
-    <loc>https://solarinvest.com/</loc>
-    <loc>https://www.instagram.com/solarinvest.br/</loc>
-    
+  ${urls}
   </urlset>`;
 
   return new Response(sitemap, {
     headers: {
-      'Content-Type': 'application/xml',
+      "Content-Type": "application/xml",
     },
   });
 }
