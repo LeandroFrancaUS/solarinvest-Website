5df116583d4e972de3e35d272edfff2e64fbde3e
Leandro Franca <101406453+LeandroFrancaUS@users.noreply.github.com>
2025-08-14 02:24:56 -0300

    add metadata to pages

--- src/app/comofunciona/page.tsx
+++ src/app/comofunciona/page.tsx
@@ -1,9 +1,13 @@
 // src/app/comofunciona/page.tsx
 
-'use client';
-
 import React from 'react';
 import ComoFunciona from '@/components/ComoFunciona';
+
+export const metadata = {
+  title: 'Como Funciona | SolarInvest',
+  description:
+    'Entenda como a SolarInvest transforma sua conta de energia em economia com um processo simples e eficiente.',
+};
 
 export default function ComoFuncionaPage() {
   return (
